4c9f0d4fe46b0b055e63b70e121753a85aa0e257
BlueLumin <78796040+BlueLumin@users.noreply.github.com>
2024-11-01 12:00:03 +0200

    Added new levels and scene changes

---
+++ level/landing_pad.gd
@@ -0,0 +1,3 @@
+extends CSGBox3D
+
+@export_file("*.tscn") var file_path # * is a placeholder for any characters.
--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,8 @@ class_name Player
 @export_range(750.0, 3000.0) var thurst: float = 1000.0 ## How much vertical force to apply when moving.
 @export var torque_thrust: float = 100.0 ## The torque thrust to apply when rotating.
 
+var is_transitioning: bool = false
+
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("boost"):
@@ -17,12 +19,33 @@ func _process(delta: float) -> void:
 
 
 func crash_sequence() -> void:
-	get_tree().call_deferred("reload_current_scene")
+	print("Crashed.")
+	set_process(false)
+	is_transitioning = true
+	
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(get_tree().reload_current_scene)
+
+
+func complete_level(next_level_file: String) -> void:
+	print("Level complete.")
+	set_process(false)
+	is_transitioning = true
+	
+	var tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_callback(
+		get_tree().change_scene_to_file.bind(next_level_file)
+	)
 
 
 func _on_body_entered(body: Node) -> void:
+	if is_transitioning:
+		return
+	
 	if body.is_in_group("Goal"):
-		print("You Win!")
+		complete_level(body.file_path)
 	
 	if body.is_in_group("Hazard"):
 		crash_sequence()
